3b8f998abc2d1b8e1dcefa3c436ec200bed1be60
joern <Gerrit@kaiMac.local>
2016-04-04 09:31:07 +0200

    blank project

---
+++ IosWeatherApp/IosWeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IosWeatherApp
+//
+//  Created by iOS Entwickler on 04.04.16.
+//  Copyright © 2016 noorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
